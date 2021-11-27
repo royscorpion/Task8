7b46948f60518d509c871223f2d5081eb547062e
Виктор Парыгин <paryginva@live.com>
2021-11-27 20:42:28 +0500

    Добавьте файлы проекта.

---
+++ Task8_1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task8_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Вывод на консоль содержимого папки на компьютере и содержимого всех подкаталогов, независимо от уровня вложенности
+            string path = "D:\\обмен\\";
+            DirectoryInfo directory = new DirectoryInfo(path);
+            if (directory.Exists)
+            {
+                Console.WriteLine("Содержимое каталога \n{0}", path);
+                GetDirFiles(directory);
+                GetSubDir(directory, path);
+            }
+            Console.ReadKey();
+        }
+
+        //Рекурсивный метод вывода содержимого каталога и его подкаталогов
+        static void GetSubDir(DirectoryInfo directory, string path)
+        {
+            DirectoryInfo[] directoryInfos = directory.GetDirectories();
+            if (directoryInfos.Length != 0)
+            {
+                foreach (DirectoryInfo d in directoryInfos)
+                {
+                    //Console.WriteLine("{0}{1}\\", path, d);
+                    GetDirFiles(d);
+                    GetSubDir(d, path + d + "\\");
+                }
+            }
+        }
+
+        //Метод вывода списка файлов в указанном каталоге
+        static void GetDirFiles(DirectoryInfo directory)
+        {
+            FileInfo[] fileInfos = directory.GetFiles();
+            foreach (FileInfo df in fileInfos)
+            {
+                Console.WriteLine(df.FullName);
+            }
+        }
+    }
+}
---
+++ Task8_2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task8_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "D:\\Calcs";
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+            if (!File.Exists(path += "\\Calc_Task8.2.txt"))
+                using (File.Create(path)) { };
+            int n = 10;
+            int Sum = 0;
+            Random random = new Random();
+            //int[] numbers = new int[n];
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    sw.WriteLine(random.Next(0, 100));
+                }
+            }
+            using (StreamReader sr = new StreamReader(path))
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    Sum += Convert.ToInt32(sr.ReadLine());
+                }
+            }
+            Console.WriteLine("Сумма случайных чисел в файле {0} составляет {1}",path,Sum);
+            Console.ReadKey();
+        }
+    }
+}
